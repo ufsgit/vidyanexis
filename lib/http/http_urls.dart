dc413f1e6f9ba48be9f43c0aa2c902701df8e104
Nihal <ufstechgithub@gmail.com>
2026-01-13 17:03:31 +0530

    quotation type api

--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -1,11 +1,11 @@
 class HttpUrls {
   //baseurls
   //live
-  static String baseUrl = 'https://vidyanexisapi.trackbox.net.in/';
+  // static String baseUrl = 'https://vidyanexisapi.trackbox.net.in/';
   // static String baseUrl = 'https://jibinsolarapi.trackbox.net.in/';
 
   //dev
-  // static String baseUrl = 'https://3pm19pm4-3511.inc1.devtunnels.ms/';
+  static String baseUrl = 'https://3pm19pm4-3511.inc1.devtunnels.ms/';
 
   static String imgBaseUrl =
       'https://pub-b2e3330da2344ea490c08dd119392728.r2.dev/';
@@ -231,4 +231,5 @@ class HttpUrls {
   static String getFollowUpStatusCustomer =
       "status/get_followupstatus_customer";
   static String leadEnquiryReport = "lead/Lead_Enquiry_Report";
+  static String getQuotationTypes = "quotation_master/get_quotation_types";
 }
