3c622208b54298f5e76397f25c0af59a8cc58654
xam-i <shamilmh@gmail.com>
2026-02-05 14:21:34 +0530

    Fixed the spacing and allignments in the customer page

--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -2,9 +2,9 @@ class HttpUrls {
   //baseurls
   //live
 //   static String baseUrl = 'https://baymentapi.trackbox.net.in/';
-  static String baseUrl = 'https://surya_prabhaapi.trackbox.net.in/';
+//  static String baseUrl = 'https://surya_prabhaapi.trackbox.net.in/';
 //   static String baseUrl = 'https://oxysolarapi.trackbox.net.in/';
-  // static String baseUrl = 'https://vidyanexisapi.trackbox.net.in/';
+  static String baseUrl = 'https://vidyanexisapi.trackbox.net.in/';
   // static String baseUrl = 'https://jibinsolarapi.trackbox.net.in/';
 //   static String baseUrl = 'https://a3secosaveapi.trackbox.net.in/';
 
