d08eb48bc75667f635b91d605971bc9cefa36cff
xam-i <shamilmh@gmail.com>
2026-02-27 15:05:45 +0530

    scroll issue in the leads page in mobile view fixed

--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -5,7 +5,7 @@ class HttpUrls {
   // static String baseUrl = 'https://bay_mentapi.trackbox.net.in/';
   // static String baseUrl = 'https://surya_prabhaapi.trackbox.net.in/';
 //  static String baseUrl = 'https://oxysolarapi.trackbox.net.in/';
-  static String baseUrl = 'https://vidyanexisapi.trackbox.net.in/';
+  // static String baseUrl = 'https://vidyanexisapi.trackbox.net.in/';
   // static String baseUrl = 'https://jibinsolarapi.trackbox.net.in/';
   // static String baseUrl = 'https://a3secosaveapi.trackbox.net.in/';
   // static String baseUrl = 'https://risingsunsolarapi.trackbox.net.in/';
@@ -13,7 +13,7 @@ class HttpUrls {
   // static String baseUrl = 'https://solarisadmin.trackbox.net.in/';
   // static String baseUrl = 'https://papscoapi.trackbox.net.in/';
   //static String baseUrl = 'https://demo3api.trackbox.net.in/';
-  // static String baseUrl = 'https://ckredencesoesyapi.trackbox.net.in/';
+  static String baseUrl = 'https://ckredencesoesyapi.trackbox.net.in/';
   // static String baseUrl = 'https://glpowerapi.trackbox.net.in/';
 
 //dev
