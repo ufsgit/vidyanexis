aa015cf44c724e7ceb0c9414d09cf73bd4b68e28
Nihal <ufstechgithub@gmail.com>
2026-02-27 15:36:57 +0530

    location at the settings

---
+++ lib/presentation/pages/settings/location_page.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:vidyanexis/constants/app_colors.dart';
+import 'package:vidyanexis/constants/app_styles.dart';
+import 'package:vidyanexis/controller/settings_provider.dart';
+import 'package:vidyanexis/presentation/pages/settings/add_location_page.dart';
+import 'package:vidyanexis/presentation/widgets/home/custom_outlined_icon_button_widget.dart';
+
+class LocationPage extends StatefulWidget {
+  const LocationPage({super.key});
+
+  @override
+  State<LocationPage> createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final settingsProvider =
+          Provider.of<SettingsProvider>(context, listen: false);
+      settingsProvider.searchLocationController.clear();
+      settingsProvider.searchLocation('', context);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double minContentWidth = 800.0;
+    final settingsProvider = Provider.of<SettingsProvider>(context);
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: SizedBox(
+            width: AppStyles.isWebScreen(context)
+                ? constraints.maxWidth < minContentWidth
+                    ? minContentWidth
+                    : constraints.maxWidth
+                : MediaQuery.of(context).size.width - 30,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Header section
+                SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Text(
+                        'Location',
+                        style: GoogleFonts.plusJakartaSans(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.textBlue800),
+                      ),
+                      const Spacer(),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 3.5,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.grey[300]!),
+                        ),
+                        child: TextField(
+                          controller: settingsProvider.searchLocationController,
+                          onChanged: (query) {
+                            settingsProvider.searchLocation(query, context);
+                            print(query);
+                          },
+                          decoration: const InputDecoration(
+                            hintText: 'Search here....',
+                            prefixIcon: Icon(Icons.search),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 4,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      CustomOutlinedSvgButton(
+                        onPressed: () async {
+                          showDialog(
+                            barrierDismissible: false,
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const AddLocation(
+                                editId: '0',
+                                isEdit: false,
+                                locationName: '',
+                              );
+                            },
+                          );
+                        },
+                        svgPath: 'assets/images/Plus.svg',
+                        label: 'New Location',
+                        breakpoint: 860,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        foregroundColor: Colors.white,
+                        backgroundColor: AppColors.primaryBlue,
+                        borderSide: BorderSide(color: AppColors.primaryBlue),
+                      ),
+                      const SizedBox(width: 16),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.surfaceGrey,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    children: [
+                      ListView.separated(
+                        separatorBuilder: (context, index) {
+                          return const SizedBox(
+                            height: 12,
+                          );
+                        },
+                        shrinkWrap: true,
+                        physics: const ClampingScrollPhysics(),
+                        itemCount: settingsProvider.locationModelList.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: AppColors.whiteColor,
+                            ),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 22,
+                                    decoration: BoxDecoration(
+                                        color: AppColors.surfaceGrey,
+                                        borderRadius:
+                                            BorderRadius.circular(12)),
+                                    child: Center(
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 8, right: 8),
+                                        child: Text(
+                                          settingsProvider
+                                              .locationModelList[index]
+                                              .locationName,
+                                          style: GoogleFonts.plusJakartaSans(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.black),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  TextButton(
+                                      onPressed: () {
+                                        showDialog(
+                                          barrierDismissible: false,
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AddLocation(
+                                              editId: settingsProvider
+                                                  .locationModelList[index]
+                                                  .locationId
+                                                  .toString(),
+                                              locationName: settingsProvider
+                                                  .locationModelList[index]
+                                                  .locationName,
+                                              isEdit: true,
+                                            );
+                                          },
+                                        );
+                                      },
+                                      child: Text(
+                                        'Edit',
+                                        style: GoogleFonts.plusJakartaSans(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600,
+                                            color: AppColors.primaryBlue),
+                                      )),
+                                  TextButton(
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title:
+                                                  const Text('Confirm Delete'),
+                                              content: const Text(
+                                                  'Are you sure you want to delete?'),
+                                              actions: [
+                                                TextButton(
+                                                  onPressed: () =>
+                                                      Navigator.pop(context),
+                                                  child: const Text('Cancel'),
+                                                ),
+                                                TextButton(
+                                                  onPressed: () async {
+                                                    await settingsProvider
+                                                        .deleteLocation(
+                                                            context,
+                                                            settingsProvider
+                                                                .locationModelList[
+                                                                    index]
+                                                                .locationId);
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: const Text(
+                                                    'Delete',
+                                                    style: TextStyle(
+                                                        color: Colors.red),
+                                                  ),
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      },
+                                      child: Text(
+                                        'Delete',
+                                        style: GoogleFonts.plusJakartaSans(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600,
+                                            color: AppColors.textRed),
+                                      ))
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
