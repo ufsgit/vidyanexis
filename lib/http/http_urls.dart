ff81b9e757d91cb4fb50f287aa265432ae0d2dc2
Aswin <153578641+workufstechnologies@users.noreply.github.com>
2026-03-09 11:12:30 +0530

    feat: Initialize core application structure, routing, and lead check-in module.

--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -20,7 +20,7 @@ class HttpUrls {
   // static String baseUrl = 'https://rythmstoreapi.trackbox.net.in/';
 
 //dev
-  static String baseUrl = 'https://3pm19pm4-3512.inc1.devtunnels.ms/';
+  static String baseUrl = 'https://2jw4dwnj-3512.inc1.devtunnels.ms/';
 
   static String imgBaseUrl =
       'https://pub-b2e3330da2344ea490c08dd119392728.r2.dev/';
