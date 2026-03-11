25aed269d53419f9ff2ff06fc4c8e511d337dcd2
Nihal <nihal12ligin@gmail.com>
2026-03-11 12:39:01 +0530

    custom fields show when edit

--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -22,8 +22,7 @@ class HttpUrls {
 //   static String baseUrl = 'https://lumiosapi.trackbox.net.in/';
 
 //dev
-  static String baseUrl = 'https://3pm19pm4-3512.inc1.devtunnels.ms/';
-  // static String baseUrl = 'https://2jw4dwnj-3512.inc1.devtunnels.ms/';
+  static String baseUrl = 'https://2jw4dwnj-3512.inc1.devtunnels.ms/';
 
   static String imgBaseUrl =
       'https://pub-b2e3330da2344ea490c08dd119392728.r2.dev/';
