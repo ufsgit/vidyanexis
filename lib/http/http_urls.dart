730cfd6ca30d9e8dab33c0e32779363f5d3a6d35
Akash jayan <officialakashjayan@gmail.com>
2026-03-17 15:45:22 +0530

    feat: Add `HttpUrls` class to centralize API endpoints and base URLs.

--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -3,7 +3,7 @@ class HttpUrls {
 //live
 
   // static String baseUrl = 'https://bay_mentapi.trackbox.net.in/';
-//   static String baseUrl = 'https://suryaprabhaapi.trackbox.net.in/';
+  //  static String baseUrl = 'https://suryaprabhaapi.trackbox.net.in/';
   // static String baseUrl = 'https://oxysolarapi.trackbox.net.in/';
   // static String baseUrl = 'https://vidyanexisapi.trackbox.net.in/';
   // static String baseUrl = 'https://jibinsolarapi.trackbox.net.in/';
@@ -12,31 +12,22 @@ class HttpUrls {
   // static String baseUrl = 'https://ecoamicaapi.trackbox.net.in/';
   // static String baseUrl = 'https://solarisadmin.trackbox.net.in/';
   // static String baseUrl = 'https://papscoapi.trackbox.net.in/';
-
   // static String baseUrl = 'https://demo3api.trackbox.net.in/';
   // static String baseUrl = 'https://demo3api.trackbox.net.in/';
-
   // static String baseUrl = 'https://ckredencesoesyapi.trackbox.net.in/';
   // static String baseUrl = 'https://glpowerapi.trackbox.net.in/';
   // static String baseUrl = 'https://ckredencesoesyapi.trackbox.net.in/';
   // static String baseUrl = 'https://lumiosapi.trackbox.net.in/';
   // static String baseUrl = 'https://rythmstoreapi.trackbox.net.in/';
   // static String baseUrl = 'https://framesolarapi.trackbox.net.in/';
-  // static String baseUrl = 'https://lumiosapi.trackbox.net.in/';
-
-//    static String baseUrl = 'https://thirdeyeapi.trackbox.net.in/';
-
-//   static String baseUrl = 'https://lumiosapi.trackbox.net.in/';
-
-
-//dev
-  static String baseUrl = 'https://3pm19pm4-3512.inc1.devtunnels.ms/';
-
   // static String baseUrl = 'https://thirdeyeapi.trackbox.net.in/';
-  // static String baseUrl = 'https://lumiosapi.trackbox.net.in/';
+ 
 
 //dev
-// static String baseUrl = 'https://3pm19pm4-3512.inc1.devtunnels.ms/';
+  // static String baseUrl = 'https://3pm19pm4-3512.inc1.devtunnels.ms/';
+  // static String baseUrl = 'https://2jw4dwnj-3512.inc1.devtunnels.ms/';
+
+
   static String baseUrl =
       'https://2jw4dwnj-3512.inc1.devtunnels.ms/https://3pm19pm4-3512.inc1.devtunnels.ms/';
 
