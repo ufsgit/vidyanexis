21d4e4c78f4c4105545535f9d222a8c5dd02cb82
Aswin <153578641+workufstechnologies@users.noreply.github.com>
2026-04-08 10:59:06 +0530

    campaign page

---
+++ lib/controller/models/campaign_model.dart
@@ -0,0 +1,43 @@
+class CampaignModel {
+  int campaignId;
+  String campaignName;
+  String campaignIdString;
+  String userIds;
+  int maxUserId;
+  String createdDate;
+  int deleteStatus;
+
+  CampaignModel({
+    required this.campaignId,
+    required this.campaignName,
+    required this.campaignIdString,
+    required this.userIds,
+    this.maxUserId = 0,
+    this.createdDate = '',
+    this.deleteStatus = 0,
+  });
+
+  factory CampaignModel.fromJson(Map<String, dynamic> json) {
+    return CampaignModel(
+      campaignId: json['Campaign_Id'] ?? 0,
+      campaignName: json['Campaign_Name'] ?? '',
+      campaignIdString: json['Campaign_Id_String'] ?? '',
+      userIds: json['User_Ids'] ?? '',
+      maxUserId: json['Max_User_Id'] ?? 0,
+      createdDate: json['Created_Date'] ?? '',
+      deleteStatus: json['DeleteStatus'] ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'Campaign_Id': campaignId,
+      'Campaign_Name': campaignName,
+      'Campaign_Id_String': campaignIdString,
+      'User_Ids': userIds,
+      'Max_User_Id': maxUserId,
+      'Created_Date': createdDate,
+      'DeleteStatus': deleteStatus,
+    };
+  }
+}
--- lib/http/http_urls.dart
+++ lib/http/http_urls.dart
@@ -24,10 +24,8 @@ class HttpUrls {
 
 //dev
 
-  static String baseUrl = 'https://3pm19pm4-3512.inc1.devtunnels.ms/';
-  // static String baseUrl = 'https://2jw4dwnj-3512.inc1.devtunnels.ms/';
-
-
+  // static String baseUrl = 'https://3pm19pm4-3512.inc1.devtunnels.ms/';
+  static String baseUrl = 'https://2jw4dwnj-3512.inc1.devtunnels.ms/';
 
   static String imgBaseUrl =
       'https://pub-b2e3330da2344ea490c08dd119392728.r2.dev/';
@@ -351,4 +349,8 @@ class HttpUrls {
   static String getCommissionReport = "lead/Get_Commission_Report";
   static String subContractsReport = "task/Sub_contracts_report";
   static String customerOutstandingReport = "lead/Customer_Outstanding_Report";
+  static String saveCampaign = "settings/Save_Campaign";
+  static String getCampaignList = "settings/Get_Campaign_List";
+  static String getCampaignById = "settings/Get_Campaign_By_Id";
+  static String deleteCampaign = "settings/Delete_Campaign";
 }
